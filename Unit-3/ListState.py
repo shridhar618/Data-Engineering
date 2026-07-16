70b1cbf4d3ebf1a39b339ea4d7538f63829e59a1
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-07-16 14:27:14 +0530

    main

--- Unit-3/ListState.py
+++ Unit-3/ListState.py
@@ -1,4 +1,24 @@
-from pyflink.finctions import KeyedProcessFunction
+from pyflink.datastream.functions import KeyedProcessFunction
 from pyflink.datastream.state import ListStateDescriptor
-from pyflink.common.typeinfo import Types
+from pyflink.common import Types
 
+
+class PurchaseHistory(KeyedProcessFunction):
+	"""A simple KeyedProcessFunction that keeps a list of purchases per key.
+
+	Note: This class is intended to be used inside a PyFlink job; the methods
+	mirror the PyFlink `KeyedProcessFunction` lifecycle (`open`,
+	`process_element`).
+	"""
+
+	def open(self, runtime_context):
+		desc = ListStateDescriptor("purchase_history", Types.INT())
+		self.history_state = runtime_context.get_list_state(desc)
+
+	def process_element(self, value, ctx):
+		# value is expected to be a tuple like (key, purchase_id)
+		self.history_state.add(value[1])
+		history = list(self.history_state.get())
+		# Return a tuple for downstream use (non-standard for Flink APIs,
+		# but useful for unit-testing this function outside of a job)
+		return [(value[0], history)]
